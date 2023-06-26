fc1f21ec345c4aa68f4b3e36e0c7152f915e6914
Setharika Sok <soksetharika7@gmail.com>
2023-06-26 13:46:39 -0400

    Complete flask set up

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,9 @@
 from app import db
+
+class Board(db.Model):
+    # board_id, int, primary key
+    # title, string
+    # owner, string
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String)
+    owner = db.Column(db.String)
