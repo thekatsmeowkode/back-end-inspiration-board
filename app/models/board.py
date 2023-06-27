c78aea74632b59389123bd1f1b0522ad8c08fc53
Setharika Sok <soksetharika7@gmail.com>
2023-06-26 23:07:03 -0400

    Created route endpoints for POST /boards and GET /boards

--- app/models/board.py
+++ app/models/board.py
@@ -1,9 +1,21 @@
 from app import db
 
 class Board(db.Model):
-    # board_id, int, primary key
-    # title, string
-    # owner, string
     board_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+    @classmethod
+    def from_dict(cls, board_data):
+        new_board = Board(
+            title = board_data['title'],
+            owner = board_data['owner']
+        )
+        return new_board
+
+    def to_dict(self):
+        return {
+            'board_id':self.board_id,
+            'title':self.title,
+            'owner':self.owner
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,32 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
+
+board_bp = Blueprint("boards", __name__, url_prefix="/boards")
+
+
+@board_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+
+    new_board = Board.from_dict(request_body)
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return {"Boards":new_board.to_dict()}, 201
+
+@board_bp.route("", methods=["GET"])
+def read_all_boards():
+    boards = Board.query.all()
+    boards_response = []
+
+    for board in boards:
+        boards_response.append(board.to_dict())
+    return jsonify(boards_response), 200
+
+
+
+
