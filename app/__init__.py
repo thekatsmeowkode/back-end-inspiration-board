15157935682f9c3ef197876825b02fffbe90d552
Kat <kmunson13@gmail.com>
2023-06-28 11:23:16 -0400

    Change URI to remote render web service

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("RENDER_DATABASE_URI")
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
